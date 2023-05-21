bbb22359b86d69ab2f6cba1902a28564d4838270
Mariia Dvoriankina <morfielmm@gmail.com>
2023-05-21 20:30:31 +0200

    lesson 18 task 2

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+import image from './assets/javascript.jpg';
+import './index.css';
+
 console.log('Hello World!');
+
+const h1 = document.createElement('h1');
+h1.textContent = 'I love JavaScript';
+document.body.append(h1);
+
+const img = document.createElement('img');
+img.setAttribute('src', image);
+document.body.append(img);
+
+
+// https://github.com/Morfielm/rollup zadanie 1
--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,26 @@
+import styles from "rollup-plugin-styles";
+import image from 'rollup-plugin-img';
+import { babel } from '@rollup/plugin-babel';
+import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload';
+
 export default {
     input: './index.js',
     output: {
-      file: './build/bundle.js',
-      format: 'cjs'
+        file: './build/bundle.js',
+        format: 'cjs',
+        assetFileNames: "[name]-[hash][extname]"
     },
     watch: {
         exclude: 'node_modules/**'
-      }
-  };
+    },
+    plugins: [
+        styles(),
+        image({
+            limit: 10000
+        }),
+        babel({ babelHelpers: 'bundled' }),
+        serve('dist'),
+        livereload()
+    ]
+};
